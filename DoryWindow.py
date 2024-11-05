f611c80d6893e11fe0827fdecd640c0ff6e3896a
Abhinav-VS21 <abhinav23bcs181@iiitkottayam.ac.in>
2024-11-06 01:39:26 +0530

    Started Setup from scratch

---
+++ DoryWindow.py
@@ -0,0 +1,105 @@
+from PySide6.QtWidgets import QMainWindow , QVBoxLayout , QHBoxLayout , QWidget , QSplitter , QApplication 
+from PySide6.QtCore import Qt , QDir
+from AddressBarWidget import AddressBarWidget
+from BookmarkTree import BookmarkTree
+from DirectoryTree import DirectoryTree
+from FileViewerWidget import FileListViewer
+from MenuBarWidget import MenuBar
+from SearchInputWidget import SearchInputWidget
+from SearchResultWidget import SearchResultWidget
+from SearchThread import SearchThread
+from StatusBarWidget import StatusBarWidget
+from catchExecptions import catch_exceptions
+import os
+import subprocess
+import platform
+import shutil
+
+class DoryWindow(QMainWindow):
+    def __init__(self , init_root_dir = QDir.homePath()):
+        super().__init__()
+
+        self.setWindowTitle("Dory")
+        self.setMinimumSize(800 , 600)
+        self.init_root_dir = init_root_dir
+        self.current_dir = init_root_dir
+
+        self.initLayout()
+        
+    def initLayout(self):
+        # Creating widgets
+        
+        self.menu_bar               = MenuBar()
+        self.address_bar_widget     = AddressBarWidget()
+        self.directory_tree         = DirectoryTree(root_directory=self.init_root_dir)
+        self.bookmark_tree          = BookmarkTree(bookmarks_file="bookmarks.json")
+        self.file_viewer            = FileListViewer(root_directory=self.init_root_dir)
+        self.search_input           = SearchInputWidget()
+        self.search_result          = SearchResultWidget()
+        self.status_bar_widget      = StatusBarWidget()
+        
+        
+        # Set size of the address bar
+        height = 40
+        self.address_bar_widget.setFixedHeight(height)
+        self.status_bar_widget.setFixedHeight(height)
+        
+        margin = 1
+        # Setting the main layout
+        directory_bookmark_widget = QWidget()
+        directory_bookmark_layout = QHBoxLayout()
+        directory_bookmark_layout.setContentsMargins(margin,margin,margin,margin)
+        directory_bookmark_widget.setLayout(directory_bookmark_layout)
+        directory_bookmark_layout.addWidget(self.directory_tree)
+        directory_bookmark_layout.addWidget(self.bookmark_tree)
+        
+        file_result_widget = QWidget()
+        file_result_layout = QHBoxLayout()
+        file_result_layout.setContentsMargins(margin , margin , margin , margin)
+        file_result_widget.setLayout(file_result_layout)
+        file_result_layout.addWidget(self.file_viewer)
+        file_result_layout.addWidget(self.search_result)
+        
+        input_search_widget = QWidget()
+        input_search_layout = QVBoxLayout()
+        input_search_layout.setContentsMargins(margin , margin , margin , margin)
+        input_search_widget.setLayout(input_search_layout)
+        input_search_layout.addWidget(self.search_input)
+        input_search_layout.addWidget(file_result_widget)
+        
+        splitter = QSplitter()
+        splitter.setHandleWidth(8)
+        splitter.setOrientation(Qt.Horizontal)
+        splitter.addWidget(directory_bookmark_widget)
+        splitter.addWidget(input_search_widget)
+        
+        central_widget = QWidget()
+        main_layout = QVBoxLayout()
+        main_layout.setContentsMargins(margin , margin , margin , margin)
+        central_widget.setLayout(main_layout)
+        main_layout.addWidget(self.address_bar_widget)
+        main_layout.addWidget(splitter)  
+        main_layout.addWidget(self.status_bar_widget)      
+        
+        
+        # Setting up the main window
+        self.setCentralWidget(central_widget)
+        self.setMenuBar(self.menu_bar)
+        
+        # Setting up the initial stage
+        self.search_input.setVisible(False)
+        self.search_result.setVisible(False)
+        self.bookmark_tree.setVisible(False)
+        self.directory_tree.setVisible(True)
+        self.file_viewer.setVisible(True)
+        self.status_bar_widget.setVisible(True)
+        self.address_bar_widget.setVisible(True)    
+        
+        
+    
+# Running Application
+if __name__ == "__main__":
+    DoryApp = QApplication([])
+    window = DoryWindow()
+    window.show()
+    DoryApp.exec()
--- Glance.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtWidgets import QApplication
-from MainWindow import MainWindow
-
-# Jai Shree Ram
-
-glanceFileManager = QApplication([])
-mainWindow = MainWindow()
-mainWindow.show()
-glanceFileManager.exec()
